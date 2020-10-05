741b68a6aa6545dab8d9582362223f911ff308f8
RachithaRai <rachitharai3016@gmail.com>
2020-10-05 14:33:31 +0530

    Update App and index

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -4,11 +4,13 @@ import Login from './Login';
 import { getTokenFromUrl } from './spotify';
 import SpotifyWebApi from 'spotify-web-api-js';
 import Player from './Player';
+import {useDataLayerValue} from './DataLayer';
 
 const spotify = new SpotifyWebApi();//creating an instance of spotify which basically responsible for the interaction between our reactapp and spotify
 
 function App() {
-  const [token, setToken] = useState(null);
+  // const [token, setToken] = useState(null);
+  const [{user, token}, dispatch] = useDataLayerValue();//to grab anything from datalayer
 
   useEffect(() => { //runs code based on a given condition
     const hash = getTokenFromUrl();
@@ -17,20 +19,48 @@ function App() {
     const _token = hash.access_token;
     
     if(_token){
-      setToken(_token);
-      spotify.setAccessToken(_token);//giving the accesstoken to the spotify api
-      spotify.getMe().then((user) => {
-        console.log('👩' , user);
-      });
-    }
 
+      dispatch({
+        type: 'SET_TOKEN',
+        token: _token,
+      });
+      // setToken(_token);
+      spotify.setAccessToken(_token);//giving the accesstoken to the spotify api
+      
+      spotify.getMe().then((user) => {
+        // console.log('👩' , user);
+        dispatch({
+          type: 'SET_USER',//action has a type
+          user: user, 
+        });//getMe gets the user and use the dispatch action to pop the user into the datalayer
+      });
+
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: 'SET_PLAYLISTS',
+          playlists: playlists,
+        });
+      });
+
+      spotify.getPlaylist('37i9dQZEVXcHRGVe9wCtY2').then(response => {
+        dispatch({
+          type: 'SET_DISCOVER_WEEKLY',
+          discover_weekly: response,
+        });
+      });
+
+    }
+    // console.log("my token", token
   }, []);//useEffect runs code based on this given condition[]
   
+  // console.log('👩' , user);
+  // console.log('👾', token);
+
   return(   
     <div className="app">
     {
       token?(
-        <Player />
+        <Player spotify={spotify} />
       ) : (
         <Login />//All the code(or *component*) for this is written in Login.js file, and we use it by first importing it in line3 
       )
